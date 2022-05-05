1c43727978eeff94978556bfd2140714b6cab842
Yuhan Jin <jyhan043@gmail.com>
2022-05-05 12:11:01 +0800

    installed mysql driver to backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 import app from "./server.js";
-import mongodb from "mongodb";
+// import mongodb from "mongodb";
+import mysql from "mysql";
 import dotenv from "dotenv";
 
-dotenv.config();
-const mongoClient = mongodb.MongoClient;
+dotenv.config();  // load settings from .env file
 const backendPort = process.env.BACKEND_PORT || 8000;  // if backend port not specified in .env file, use 8000
+
+var connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected to database.');
+});
+
+connection.end();
