55e0ad43069a8d1c03715175116dcd7316d6e86f
Yuhan Jin <jyhan043@gmail.com>
2022-05-05 12:47:38 +0800

    trying DB connection, no luck

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,22 @@ import dotenv from "dotenv";
 dotenv.config();  // load settings from .env file
 const backendPort = process.env.BACKEND_PORT || 8000;  // if backend port not specified in .env file, use 8000
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host     : process.env.RDS_HOSTNAME,
     user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT
 });
 
-connection.connect(function(err) {
+connection.connect((err) => {
     if (err) {
         console.error('Database connection failed: ' + err.stack);
         return;
     }
-
     console.log('Connected to database.');
+    app.listen(backendPort, () => {
+        console.log(`Example app listening on port ${backendPort}`)
+    });
 });
 
 connection.end();
